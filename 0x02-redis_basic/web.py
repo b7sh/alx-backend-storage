74839d5c8a67c0ac6d81c7d37d66d7d42afef040
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-23 14:53:34 +0000

    solve the advanced task for the seventh time

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -49,7 +49,4 @@ def get_page(url: str) -> str:
 
 
 if __name__ == "__main__":
-    get_page(
-        '''http://slowwly.robertomurray.co.uk/
-        delay/5000/url/http://www.google.com'''
-        )
+    get_page('http://www.google.com')
