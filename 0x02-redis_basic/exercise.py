37ba7e0e56f3120e267e2bed9bb1573e15a26db6
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-23 11:59:09 +0000

    solve the first task errors '0. Writing strings to Redis'

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,11 +14,19 @@ import redis
 from typing import Union
 import uuid
 
+
 class Cache:
+    'the cache class'
     def __init__(self):
+        '''
+        attributes:
+            _redis: Object
+        '''
         self._redis = redis.Redis()
         self._redis.flushdb()
+
     def store(self, data: Union[str, float, int, bytes]) -> str:
+        'takes a data argument and returns a string.'
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
