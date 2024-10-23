e42f08a01e132a91e621cdadb4523c8d71ed25fd
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-23 12:57:04 +0000

    solve the second task '1. Reading from Redis and recovering original type'

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 ''' Redis task '''
 import redis
-from typing import Union
-import uuid
+from typing import Union, Optional, Callable
+from uuid import uuid4
 
 
 class Cache:
@@ -12,8 +12,30 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, int, bytes]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         'takes a data argument and returns a string.'
-        random_key = str(uuid.uuid4())
+        random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        ''' Reading from redis '''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(vlue)
+        return value
+
+    def get_str(self, key: str) -> str:
+        ''' parameterize a value from redis to str '''
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        ''' parameterize a value from redis to int '''
+        value = self.redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
