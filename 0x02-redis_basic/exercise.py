8bd9f8d56ee7ee12b256737da40a042a9f0b2dea
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-23 11:54:06 +0000

    solve the first task '0. Writing strings to Redis'

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/eenv python3
+'''
+Create a Cache class. In the __init__ method,
+store an instance of the Redis client as
+a private variable named _redis (using redis.Redis())
+and flush the instance using flushdb.
+Create a store method that takes a data argument
+and returns a string. The method should generate
+a random key (e.g. using uuid),
+store the input data in Redis using
+the random key and return the key.
+'''
+import redis
+from typing import Union
+import uuid
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    def store(self, data: Union[str, float, int, bytes]) -> str:
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
